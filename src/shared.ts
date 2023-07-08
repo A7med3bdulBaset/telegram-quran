4f269b2c5076234097b9874735aba5e209f77b88
A7med3bdulBaset <A7med3bdulBaset@gmail.com>
2023-07-09 00:40:54 +0300

    export often used vars

---
+++ src/shared.ts
@@ -0,0 +1,35 @@
+/** Shared variables */
+
+import { _callbacks } from "./redux/actions";
+import state from "./redux/store";
+
+/** API breakpoints */
+
+const rootApi = "https://api.quran.com/api/v4/";
+
+export const API = {
+	/** api.quran.com */
+	root: rootApi,
+	/** Get a recitation of a surah by reciter id and surah id */
+	reciteSurah: (reciterId: number, surahId: number) =>
+		`${rootApi}chapter_recitations/${reciterId || 12}/${surahId}`,
+	/** Get a text of a verse by surah id and verse id */
+	ayahText: (surahId: number, verseId: number) =>
+		`${rootApi}quran/verses/imlaei?verse_key=${surahId}:${verseId}`,
+	/** Get a recitation of a verse by reciter id, surah id and verse id */
+	reciteAyah: (reciterId: number, surahId: number, verseId: number) => {
+		return `${rootApi}recitations/${reciterId}/by_ayah/${surahId}:${verseId}`;
+	},
+	/** audio.qurancdn.com */
+	audioHost: "https://audio.qurancdn.com/",
+	/** Get a tafsir of a verse by surah id and verse id */
+	tafsirAyah: (surahId: number, verseId: number) =>
+		`https://api.qurancdn.com/api/v4/tafsirs/16/by_ayah/${surahId}:${verseId}?locale=ar`,
+	// 16 is the id of altafsir almoysser
+};
+
+const users = state.getState();
+const getCurrentUser = (userId: number) => users[userId.toString()] || null;
+
+export { state, users, getCurrentUser, _callbacks };
+export { setCurrentAction, resetActions, createNewUser } from "./redux/slice";
